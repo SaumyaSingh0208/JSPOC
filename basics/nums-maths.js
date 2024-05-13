888da51fe0de0750f3f073a7a200cf4c9e9953f4
SaumyaSingh0201 <saumya.singh@mheducation.com>
2024-05-14 01:19:15 +0530

    Basicsnums and math

---
+++ basics/nums-maths.js
@@ -0,0 +1,37 @@
+// const score = 400;
+// console.log(score); //400
+
+// const balance = new Number(100);
+// console.log(balance); //[Number: 100]
+
+// console.log(balance.toString().length); //3
+// console.log(balance.toFixed(2)); //100.00
+
+// const num= 123.8966
+// console.log(num.toPrecision(3)); //124
+
+// const hundreds = 1000000000;
+// console.log(hundreds.toLocaleString()); //1,000,000,000
+// //Makes it readable but is in US standard so comma is placed after 3 digits
+
+// console.log(hundreds.toLocaleString('en-IN'));  //1,00,00,00,000
+// //To convert it according to Indian standards
+
+//********************************* Maths *********************/
+console.log(Math); //Object [Math] {}
+console.log(Math.abs(-4)); //4 => converts negative number to positive
+console.log(Math.round(4.6)); //5
+console.log(Math.ceil(4.6)); //5
+console.log(Math.floor(4.6)); //4
+
+console.log(Math.random()); //Always gives values betweem 0 and 1
+console.log((Math.random()*10)+1);
+console.log(Math.floor((Math.random()*10))+1); //Always value is above 10
+//Suppose value is 0.1 so value returned by floor will be 0 so 1 is added to make it between 1 and 9
+
+
+const min =10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min) //Output is always above 10
+
--- basics/strings.js
+++ basics/strings.js
@@ -1,5 +1,57 @@
+// Strings can be created as primitives, from string literals, or as objects, using the String() constructor:
+const string1 = "A string primitive";
+const string2 = 'Also a string primitive';
+const string3 = `Yet another string primitive`;
+
+const string4 = new String("A String object");
+console.log(typeof(string1)); //Output => string
+console.log(typeof(string2)); //Output => string
+console.log(typeof(string3)); //Output => string
+console.log(typeof(string4)); //Output => object
+
+// Character access
+// There are two ways to access an individual character in a string. The first is the charAt() method:
+"cat".charAt(1); // gives value "a"
+
+//The other way is to treat the string as an array-like object, where individual characters correspond to a numerical index:
+"cat"[1]; // gives value "a"
+
+
+
 const name ="Saumya"
 const projectCount =70
 
 console.log(`Hello my name is ${name} and my project count is ${projectCount}`)
-//This is called string interpolation 
+//This is called string interpolation and writte in backticks(``)
+
+const getCourse = new String('ReactJS')
+// Another way of declaring string which provide methods of string
+// console.log(getCourse.__proto__); // To see methods
+// console.log(getCourse.length);
+// console.log(getCourse.toUpperCase());
+// console.log(getCourse.indexOf('t'));
+
+// //To divide in substring
+// const newString= getCourse.substring(0,4); // Last value does not get included -> Output - Reac
+// console.log(newString);
+
+// const anotherString = getCourse.slice(-6,4); //Output => eac
+// console.log(anotherString);
+
+// const anotherStringOne = getCourse.slice(-6,4); //Output => ea
+// console.log(anotherStringOne);
+
+// const anotherStringTwo = getCourse.slice(-7,4); //Output => Reac
+// console.log(anotherStringTwo);
+
+// const anotherStringThree = getCourse.slice(-5,4); //Output => ac
+// console.log(anotherStringThree);
+
+const stringWithSpace ="  Saumya "
+console.log(stringWithSpace);  //Output =>   Saumya 
+//To remove white space we use function trim()
+console.log(stringWithSpace.trim()); // Output =>Saumya
+
+const url ="https://saumya0208.com/saumya%20singh"
+console.log(url.replace('%20',' '));  //Output=> https://saumya0208.com/saumyasingh
+console.log(url.includes('saumya'));  //Output=>true
